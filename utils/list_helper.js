c5829e9922a05fea0ae729ba270f540c4fe04c46
Jarmo Kuusisto <jarmokuu@gmail.com>
2018-12-11 00:56:01 +0200

    4_7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -23,9 +23,65 @@ const favoriteBlog = (blogs) => {
     console.log(id)
     return blogs.filter(b => b._id === id)
 }
+
+const mostBlogs = (blogs) => {
+    let authors = []
+    blogs.forEach(b => {
+        if(authors.some(function(x) { return x.author === b.author})){
+            let foundIndex = authors.findIndex(i => i.author === b.author)
+            authors[foundIndex].blogs += 1
+        } else {
+            let newAuthor = {}
+            newAuthor.author = b.author
+            newAuthor.blogs = 1
+            authors.push(newAuthor)
+        }
+    })
+
+    let eniten = 0
+    let author = ''
+
+    authors.forEach(element => {
+        if(element.blogs > eniten){
+            eniten = element.blogs
+            author = element.author
+        }
+    })
+
+    return authors.filter(a => a.author === author)
+}
+
+const mostLikes = (blogs) => {
+    let authors = []
+    blogs.forEach(b => {
+        if(authors.some(function(x) { return x.author === b.author})){
+            let foundIndex = authors.findIndex(i => i.author === b.author)
+            authors[foundIndex].likes += b.likes
+        } else {
+            let newAuthor = {}
+            newAuthor.author = b.author
+            newAuthor.likes = b.likes
+            authors.push(newAuthor)
+        }
+    })
+
+    let eniten = 0
+    let author = ''
+
+    authors.forEach(element => {
+        if(element.likes > eniten){
+            eniten = element.likes
+            author = element.author
+        }
+    })
+
+    return authors.filter(a => a.author === author)
+}
   
   module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
