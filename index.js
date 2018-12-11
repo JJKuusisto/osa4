c3c4bc5462ead36255272872c3037fe1e7de002d
Jarmo Kuusisto <jarmokuu@gmail.com>
2018-12-11 06:21:48 +0200

    4_11

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,30 +6,43 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
 
 
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(middleware.logger)
-app.use('/api/blogs', blogsRouter)
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
   
   mongoose
-    .connect(process.env.MONGODB_URI)
-    .then( () => {
-      console.log('connected to database', process.env.MONGODB_URI)
-    })
-    .catch( err => {
-      console.log(err)
-    })
+  .connect(config.mongoUrl)
+  .then( () => {
+    console.log('connected to database', config.mongoUrl)
+  })
+  .catch( err => {
+    console.log(err)
+  })
+  
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(middleware.logger)
+  app.use('/api/blogs', blogsRouter)
+
+  app.use(middleware.error)
 
 
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const server = http.createServer(app)
+
+  server.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`)
+  })
+  
+  server.on('close', () => {
+    mongoose.connection.close()
+  })
+  
+  module.exports = {
+    app, server
+  }
